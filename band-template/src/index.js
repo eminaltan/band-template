1651ede0e157f479b522cf17484cc725ab87b535
Emin Altan <emin.altan@hotmail.com>
2023-08-31 17:55:23 +0300

    <Main/> component added

--- band-template/src/index.js
+++ band-template/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Main from "./components/Main";
 /* import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'; */
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
-    {/*     <App /> */}
+    <Main />
   </React.StrictMode>
 );
 
